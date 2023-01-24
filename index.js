b7d6ea6f92af0f84ec7477e1e1db41c97be2303b
Vitor Castilho <112523925+vcastw@users.noreply.github.com>
2023-01-24 14:08:04 -0300

    sorteador

---
+++ index.js
@@ -0,0 +1,19 @@
+const sorteador = () => {
+    const valorMinimo = document.getElementById('valorMinimo');
+    const valorMaximo = document.getElementById('valorMaximo');
+    const resultado = document.querySelector('.resultado');
+
+    const sortear = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    document.addEventListener('click', (event) => {
+        const elemento = event.target;
+        if (elemento.classList.contains('sortear')) {
+            resultado.innerHTML = sortear(Number(valorMinimo.value), Number(valorMaximo.value));
+        };
+    });
+};
+sorteador();
