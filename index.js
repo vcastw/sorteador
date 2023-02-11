94ad638a7591353ae834a092c9a3be491143c859
Vitor Castilho <112523925+vcastw@users.noreply.github.com>
2023-02-11 20:59:35 -0300

    sorteador

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sorteador = () => {
     const sortear = (min, max) => {
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
     document.addEventListener('click', (event) => {
